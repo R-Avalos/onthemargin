7edfbc8661e450f29e0f624b96686bb00fc95b9c
Randy Avalos <randy.joseph.avalos@gmail.com>
2017-06-29 16:56:44 -0700

    Added test plot and control

--- ravalos_dashboard_app.r
+++ ravalos_dashboard_app.r
@@ -3,12 +3,41 @@ library('shinydashboard')
 library('shiny')
 
 ui <- dashboardPage(
-        dashboardHeader(),
-        dashboardSidebar(),
-        dashboardBody()
+        dashboardHeader(title = "Randy Avalos"),
+        dashboardSidebar(
+                menuItem(text = "Github", 
+                         tabName = "Github", 
+                         icon = icon("github"),
+                         badgeLabel = "Commit History",
+                         badgeColor = "teal"
+                         )
+        ),
+        dashboardBody(
+                # Github tab
+                tabItem(tabName = "Github",
+                        fluidRow(
+                                box(plotOutput("testPlot", height = 250)),
+                                box(
+                                        title = "testPlot Controls",
+                                        sliderInput("slider", "Obs Count:", 1, 100, 50)
+                                )
+                        )
+                        )
+        )
 )
 
+shinyApp(ui, server) #preview dashboard
+
+
 server <- function(input,output){
+        set.seed(42)
+        histdata <- rnorm(500)
+        
+        output$testPlot <- renderPlot(
+                {
+                data <- histdata[seq_len(input$slider)]
+                hist(data)
+        }
+        )
 }
 
-shinyApp(ui, server) #preview dashboard

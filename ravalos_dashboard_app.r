61274d58330029aa9717adff6bb07651927ba434
Randy Avalos <randy.joseph.avalos@gmail.com>
2017-07-19 20:21:54 -0700

    Added columns to shinydashboard to provide margins for elements

--- ravalos_dashboard_app.r
+++ ravalos_dashboard_app.r
@@ -1,11 +1,14 @@
 # Shiny Dashboard App
 
 # Potential data sources:
-# github, cryptocurrency, f1 racing, SFMoMA
+# github, cryptocurrency, f1 racing, SFMoMA, NYPL Menus
+# https://www.rstudio.com/resources/webinars/dynamic-dashboards-with-shiny/
 
+# 12 rows that can be divided as needed ... fluidrow(column(width = 12))
+# adding column value provdes a margin
 
-
-
+# infoBox() for small amounts of info
+# valueBox() for small amounts of info
 
 library('shinydashboard')
 library('shiny')
@@ -19,27 +22,28 @@ ui <- dashboardPage(
                          badgeLabel = "Commit History",
                          badgeColor = "teal"
                          )
-        ),
+                ),
         dashboardBody(
                 # Github tab
                 tabItem(tabName = "Github",
-                        fluidRow(
-                                box(plotOutput("testPlot", height = 250)),
-                                box(
-                                        title = "testPlot Controls",
-                                        sliderInput("slider", "Obs Count:", 1, 100, 50)
-                                )
-                        ),
-                        fluidRow(
-                                box(plotOutput("testPlot2", height = 250)),
-                                box(
-                                        title = "tesPlot2 Controls",
-                                        sliderInput("slider2", "Count", 1, 500, 100)
-                                )
+                        fluidRow(column(width = 6, 
+                                         box(plotOutput("testPlot", height = 250)),
+                                         box(title = "testPlot Controls",
+                                             sliderInput("slider", "Obs Count:", 1, 100, 50)
+                                             )
+                                        )
+                                 ),
+                        fluidRow(column(width = 6,
+                                        box(plotOutput("testPlot2", height = 250)),
+                                        box(
+                                                title = "tesPlot2 Controls",
+                                                sliderInput("slider2", "Count", 1, 500, 100)
+                                                )
+                                        )
+                                 )
                         )
-                        )
+                )
         )
-)
 
 shinyApp(ui, server) #preview dashboard
 

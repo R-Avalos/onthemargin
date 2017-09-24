ec6ec30bb9454a1fa98d4f8b9f16e098d2026f93
Randy Avalos <randy.joseph.avalos@gmail.com>
2017-09-24 13:19:25 -0700

    Added data table to shiny app

--- test_dash.R
+++ test_dash.R
@@ -5,6 +5,23 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 
+## Functions
+googleSearchLink <- function(val) {
+        sprintf('<a href="https://www.google.com/#q=%s" target="_blank" class="btn btn-primary">Info</a>',val)
+} # google search
+
+createLink <- function(link, reference) {
+        paste0('<a href=', link, ">", reference, "</a>")
+} # pilot link
+
+bessel_results[1,]
+bessel_results$Pilot_link[1]
+bessel_results$Pilot.Handle[1]
+createLink(link = bessel_results$Pilot_link[1], reference = bessel_results$Pilot.Handle[1])
+
+
+## Dashboard
+
 ui <- dashboardPage(
         skin = "green",
         dashboardHeader(title = "MultiGP Drone Racing"),
@@ -12,68 +29,45 @@ ui <- dashboardPage(
                 sidebarMenu(
                         menuItem(text = "Bessel Run", 
                                  tabName = "bessel"
-                                 )
+                                 ),
+                        menuItem(text = "Bessel Table",
+                                 tabName = "bessel_table")
                         )
                 ),
         dashboardBody(
                 tabItems(
-                        tabItem(tabName = "bessel",
-                                h2("Bessel Run, Race Times"),
-                                fluidRow(
-                                        
-                                        radioButtons(
-                                                inputId = "radio",
-                                                label = "Selection Type:",
-                                                choices = list(
-                                                        "All",
-                                                        "Manual Select"
-                                                ),
-                                                selected = "All"
-                                        ),
-                                        
-                                        selectizeInput("select_chapter_input",
-                                                    label = "Chapters",
-                                                    choices = bessel_results$Chapter,
-                                                    selected = bessel_results$Chapter[bessel_results$Time == min(bessel_results$Time)],
-                                                    multiple = TRUE)
-                                        
-                                ),
-                                plotOutput(
-                                        "bessel_plot", 
-                                        width = "auto", height = "600px",
-                                        hover = hoverOpts(id = "bessel_hover")   
-                                        )
+                        # tabItem(tabName = "bessel",
+                        #         h2("Bessel Run, Race Times"),
+                        #         plotOutput(
+                        #                 "bessel_plot", 
+                        #                 width = "auto", height = "600px")   
+                        #                 )
+                        #         ),
+                        tabItem(tabName = "bessel_table",
+                                h2("Bessel Run, Table of Results"),
+                                
+                                dataTableOutput(outputId = "bessel_table")
                                 )
-                        )
                 )
+        )
 )
 
-server <- function(input,output){
-        output$bessel_plot <- renderPlot({
-                plot_data <- reactive({
-                        if(input$radio == "All"){
-                                bessel_results
-                        }
-                        else {
-                        bessel_results %>%
-                                filter(Chapter == input$select_chapter_input)
-                        }
-                })
-                        
 
-                bessel_plot <- ggplot(data = plot_data(), 
-                                      aes(x = Time, y = Chapter, color = Date.Recorded)) +
-                        geom_vline(xintercept = min(plot_data()$Time), 
-                                   color = "red", 
-                                   alpha = 0.5, 
-                                   size = 1) +
-                        geom_point(alpha = 0.5, size =2) +
-                        scale_y_discrete(limits = rev(levels(y$Chapter))) +
-                        scale_x_continuous(breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80),
-                                           limits = c(0, max(y$Time+5))) +
-                        theme_tufte()
-                print(bessel_plot)
-        })
+
+server <- function(input,output){
+        output$bessel_table <- renderDataTable({bessel_results})
+        
+        # output$bessel_plot <- ggplot(data = bessel_results, 
+        #                       aes(x = Time, y = Chapter, color = Date.Recorded)) +
+        #         geom_vline(xintercept = min(bessel_results$Time), 
+        #                    color = "red", 
+        #                    alpha = 0.5, 
+        #                    size = 1) +
+        #         geom_point(alpha = 0.5, size =2) +
+        #         scale_y_discrete(limits = rev(levels(bessel_results$Chapter))) +
+        #         scale_x_continuous(breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80)) +
+        #         limits = c(0, max(bessel_results$Time+5)) +
+        #         theme_tufte()
 }
 
 shinyApp(ui, server) #preview dashboard
